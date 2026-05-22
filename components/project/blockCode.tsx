122d52d64760057c2a351b1d75f5f9384ebaf05e
meongganas <farreluken@gmail.com>
2026-05-22 23:26:53 +0800

    fix overflow

--- components/project/blockCode.tsx
+++ components/project/blockCode.tsx
@@ -5,7 +5,7 @@ import { TokensResult } from 'shiki';
 function CodeBlockRenderer({ tokens }: { tokens: TokensResult }) {
     return (
         <CodeBlock tokens={tokens}>
-            <CodeBlock.Code className="bg-gray-50 p-6">
+            <CodeBlock.Code className="bg-gray-50 p-6 overflow-x-auto">
                 <div className="table-row">
                     <CodeBlock.LineNumber className="table-cell pr-4 text-sm text-gray-500 text-right select-none" />
                     <CodeBlock.LineContent className="table-cell">
