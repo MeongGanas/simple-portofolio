905f26f1ca24ae918771979ec4148c12c1cb7b35
meongganas <farreluken@gmail.com>
2026-05-22 23:29:21 +0800

    change font size

--- components/project/blockCode.tsx
+++ components/project/blockCode.tsx
@@ -8,7 +8,7 @@ function CodeBlockRenderer({ tokens }: { tokens: TokensResult }) {
             <CodeBlock.Code className="bg-gray-50 p-6 overflow-x-auto">
                 <div className="table-row">
                     <CodeBlock.LineNumber className="table-cell pr-4 text-sm text-gray-500 text-right select-none" />
-                    <CodeBlock.LineContent className="table-cell">
+                    <CodeBlock.LineContent className="table-cell text-sm sm:text-base">
                         <CodeBlock.Token />
                     </CodeBlock.LineContent>
                 </div>
